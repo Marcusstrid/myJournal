ef8169abe27c4d335c93e1789a2a88acfaf42096
Marcusstrid <marcus95strid@gmail.com>
2018-05-21 21:16:06 +0200

    a
    a

--- config.php
+++ config.php
@@ -1,13 +1,15 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost:8888');
+define('DB_SERVER', 'localhost:8889');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'demo');
+define('DB_PASSWORD', 'root');
+define('DB_NAME', 'Journal');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+$db = new mysqli('localhost','root','root', 'Journal') or die('error with connection');
  
 // Check connection
 if($link === false){
--- welcome.php
+++ welcome.php
@@ -1,13 +1,40 @@
+
 <?php
-// Initialize the session
 session_start();
+
+
  
 // If session variable is not set it will redirect to login page
 if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
   header("location: login.php");
   exit;
 }
+
+
+if(isset($_POST['title'])){
+    $title = $_POST['title'];
+    $content = $_POST['content'];
+    $title = $db->real_escape_string($title);
+    $content = $db->real_escape_string($content);
+    $userID = $_SESSION['userID'];
+    $date = createdAt('Y-m-d G:i:s');
+    $content = htmlentities($content);
+
+    if($title && $content){
+        $query = $db->query("INSERT INTO entries (userID, title, content, createdAt) VALUES('$userID', '$title', '$content', '$createdAt')");
+        if($query){
+            echo "post added";
+        }else{
+            echo "error";
+        }
+    }else{
+        echo "missing data";
+    }
+        
+    }
+
 ?>
+
  
 <!DOCTYPE html>
 <html lang="en">
@@ -15,8 +42,11 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
     <meta charset="UTF-8">
     <title>Welcome</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
-        body{ font: 14px sans-serif; text-align: center; }
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+   
+   
+
+        
     </style>
 </head>
 <body>
@@ -24,5 +54,18 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
         <h1>Hi, <b><?php echo htmlspecialchars($_SESSION['username']); ?></b>. Welcome to our site.</h1>
     </div>
     <p><a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a></p>
+
+
+    <div id="wrapper">
+         <div id ="content">
+             <form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
+             <label>Title:</label><input type="text" name="title"/>
+             <label for="content">Content:</label>
+             <textarea name="content" cols=50 rows=10></textarea>
+             <br />
+             <input type="submit" name="submit" value="Submit">
+              </form>
+         </div>
+    </div>
 </body>
 </html>
