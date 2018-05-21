46aa7ee8280097317aec63194e63cfca5dc85ee8
Marcusstrid <marcus95strid@gmail.com>
2018-05-21 14:36:37 +0200

    hello
    dad

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php require_once 'head.php';
-
-if (!isset($_SESSION["loggedIn"])); ?>
-<form action="partials/login.php" method="POST">
-    <input type="text" placeholder="username" name="username"/>
-    <input type="text" placeholder="password" name="password"/>
-    <input type="submit" value="Skicka" />
-</form>
