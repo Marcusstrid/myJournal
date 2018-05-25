25d43666367d0614fb02dd608c6f5e78a69658dc
Marcusstrid <marcus95strid@gmail.com>
2018-05-25 13:18:29 +0200

    a
    a

--- partials/config.php
+++ partials/config.php
@@ -1,6 +1,4 @@
 <?php
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
 
 ob_start();
 session_start();
@@ -10,17 +8,16 @@ define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'root');
 define('DB_NAME', 'Journal');
  
-/* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-// $db = new PDO("mysql : host=".DBHOST.";port=8889; dbname=".DBNAME, DBUSER, DBPASS);
-// $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+//kolla db connection
 
-
- 
-// Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+
+
 ?>
+
+
--- partials/get_all_entries.php
+++ partials/get_all_entries.php
@@ -3,23 +3,35 @@
 
 include('config.php');
 
-
 $result = mysqli_query($link,"SELECT * FROM entries");
 
 echo "<table border='1'>
 <tr>
+<th>ID</th>
 <th>title</th>
 <th>content</th>
+
 </tr>";
 
 while($row = mysqli_fetch_array($result))
 {
 echo "<tr>";
+echo "<td>" . $row['id'] . "</td>";
 echo "<td>" . $row['title'] . "</td>";
 echo "<td>" . $row['content'] . "</td>";
+echo '<td><a href="edit.php?id=' . $row['id'] . '">Edit</a></td>';
+echo '<td><a href="welcome.php?id=' . $row['id'] . '">Delete</a></td>';
+ 
 echo "</tr>";
+
 }
 echo "</table>";
 
+//include('delete.php');
+//include('edit.php');
+
 mysqli_close($link);
+
+
 ?>
+
